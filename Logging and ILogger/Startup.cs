dc84d65da77a054cc9506c34a4657637b2c7049d
MaxIvanov44 <Lvanovmaksim200033@gmail.com>
2020-01-28 13:28:36 +0300

    Добавьте файлы проекта.

---
+++ Logging and ILogger/Startup.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Logging_and_ILogger
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, ILogger<Program> logger)
+        {
+            app.Run(async (context) =>
+            {
+                logger.LogInformation("Processing request {0}", context.Request.Path);
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
